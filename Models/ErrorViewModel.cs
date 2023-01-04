256867cf0b09c82ae61125bc4182fcceb9d9a27a
NguyenThaoNguyen1 <nguyenthaonguyen20102001@gmail.com>
2023-01-04 08:35:35 +0700

    first_commit_NTN451

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThaoNguyen451.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
